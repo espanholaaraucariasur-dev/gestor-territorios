44b16dc32e92b312d4642cda2a0a85de60794060
Dev Araucaria Sur <dev@araucariasur.app>
2026-05-09 00:41:29 +0000

    fix: biometria - MainActivity extiende FlutterFragmentActivity
    local_auth requiere FlutterFragmentActivity en lugar de FlutterActivity

--- android/app/src/main/kotlin/com/congregacion/araucaria_sur/MainActivity.kt
+++ android/app/src/main/kotlin/com/congregacion/araucaria_sur/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.congregacion.araucaria_sur
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
